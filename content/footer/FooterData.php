00bd3dde124e2d800f53361fa1d623ef0bd15190
Cody Logan <clpo13@gmail.com>
2018-02-21 09:07:21 -0800

    Add board revision data to footer

--- content/footer/FooterData.php
+++ content/footer/FooterData.php
@@ -24,6 +24,9 @@ class FooterData implements CurrantModule
         $name_full = trim(preg_replace('/\s\s+/', ' ', shell_exec('cat /proc/cpuinfo | grep name | head -1')));
         $name = explode(': ', $name_full);
 
+				// find Rasbperry Pi revision and model
+        $revision = trim(preg_replace('/\s\s+/', ' ', shell_exec("cat /proc/cpuinfo | grep 'Revision' | awk '{print $3}' | sed 's/^1000//'")));
+
         //determine php version
         $php_version = explode('.', phpversion());
 
@@ -33,6 +36,7 @@ class FooterData implements CurrantModule
         $data->webserver = $_SERVER['SERVER_NAME'].' ('.$_SERVER['SERVER_ADDR'].') - '.$_SERVER['SERVER_SOFTWARE'];
         $data->php_version = 'PHP/' . $php_version[0] . '.' . $php_version[1] . '.' . $php_version[1];
         $data->cpu = $name[1];
+        $data->revision = $revision;
 
         return $data;
     }
--- content/footer/FooterView.php
+++ content/footer/FooterView.php
@@ -13,6 +13,7 @@ $footer_data = $footer->getData();
 
 <p><?php echo $footer_data->webserver; ?> - <?php echo $footer_data->php_version; ?></p>
 <p><?php echo $footer_data->cpu; ?></p>
+<p><?php echo $footer_data->revision; ?></p>
 
 <p>
   <a href="https://github.com/ColinWaddell/CurrantPi"><i class="demo-icon icon-github"></i></a>
