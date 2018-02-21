f4a03393349c2caf343b9517bcd2cd588c30d4b5
Cody Logan <clpo13@gmail.com>
2018-02-21 09:25:31 -0800

    Map revision number to model

--- content/footer/FooterData.php
+++ content/footer/FooterData.php
@@ -24,8 +24,45 @@ class FooterData implements CurrantModule
         $name_full = trim(preg_replace('/\s\s+/', ' ', shell_exec('cat /proc/cpuinfo | grep name | head -1')));
         $name = explode(': ', $name_full);
 
+        // match revisions to model names
+        $model_arr = array(
+          "Beta" => "B (Beta)",
+          "0002" => "B",
+          "0003" => "B (ECN0001)",
+          "0004" => "B",
+          "0005" => "B",
+          "0006" => "B",
+          "0007" => "A",
+          "0008" => "A",
+          "0009" => "A",
+          "000d" => "B",
+          "000e" => "B",
+          "000f" => "B",
+          "0010" => "B+",
+          "0011" => "Compute Module 1",
+          "0012" => "A+",
+          "0013" => "B+",
+          "0014" => "Compute Module 1",
+          "0015" => "A+",
+          "a01040" => "2 Model B",
+          "a01041" => "2 Model B",
+          "a21041" => "2 Model B",
+          "a21042" => "2 Model B (with BCM2837)",
+          "900021" => "A+",
+          "900032" => "B+",
+          "900092" => "Zero",
+          "900093" => "Zero",
+          "920092" => "Zero",
+          "9000c1" => "Zero W",
+          "a02082" => "3 Model B",
+          "a020a0" => "Compute Module 3/CM3 Lite",
+          "a22082" => "3 Model B",
+          "a32082" => "3 Model B",
+        );
+
 				// find Rasbperry Pi revision and model
         $revision = trim(preg_replace('/\s\s+/', ' ', shell_exec("cat /proc/cpuinfo | grep 'Revision' | awk '{print $3}' | sed 's/^1000//'")));
+        $model = $model_arr[$revision];
 
         //determine php version
         $php_version = explode('.', phpversion());
@@ -37,6 +74,7 @@ class FooterData implements CurrantModule
         $data->php_version = 'PHP/' . $php_version[0] . '.' . $php_version[1] . '.' . $php_version[1];
         $data->cpu = $name[1];
         $data->revision = $revision;
+        $data->model = $model;
 
         return $data;
     }
